9ac021845e6c1e24cda465a316a20cad3c43c545
R0tenur <himself@jenshunt.se>
2021-08-16 22:59:47 +0200

    docs: add sample

---
+++ sample/Color.Sample/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using Color;
+
+Console.WriteLine("Hello".Color("#b3b97e").Bold() + " World".Color("#ff0000").StrikeThrough());
+Console.WriteLine($"What {"Is".Red().BgDarkGreen().Italic()} this? {"Dunno".Dim()} You?");
